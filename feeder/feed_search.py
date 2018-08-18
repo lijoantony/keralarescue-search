13ac28ac137ff019a61ee8a353c23a2c7eca7cf8
Ajitsen Surendran <ajitsen_s@yahoo.co.in>
2018-08-18 14:31:04 +0530

    feed search - need testing

--- feeder/feed_search.py
+++ feeder/feed_search.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
-import urllib
 import datetime
+import os
+import urllib
+
 import pandas as pd
+
 now = datetime.datetime.now()
 run_id = now.strftime("%m%d_%H%M")
 
@@ -9,17 +12,28 @@ run_id = now.strftime("%m%d_%H%M")
 # DATA_FILE = "../data_" + run_id # FIXME - now the url returns 403
 # avoid download if file already downloaded in last 10mins
 
+customHeader = {
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Connection": "keep-alive",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36"
+}
+
 
 def save_data_json(url="https://keralarescue.in/data/", fetch=False):
-    json_file = "../data.json"
     if fetch:
-        json_file = "../data_" + run_id + ".json"
-        urlFile = urllib.URLopener()
-        urlFile.retrieve(url, json_file)
-    return json_file
+        json_file1 = "../data_" + run_id + ".json"
+        request = urllib.request.Request(url, headers=customHeader)
+        json_str = urllib.urlopen(request).read()
+        with open(json_file1, "w") as fh:
+            fh.write(json_str)
+            return json_file1
+    else:
+        value = os.popen('ls -tr ../data* | tail -1').readlines()
+        return str.strip(str(value[-1]))
 
 
-def getDistPlace(dist, loc):
+def get_dist_place(dist, loc):
     return loc + "(" + dist + ")"
 
 
@@ -27,8 +41,8 @@ def convert_json_to_csv(input_json):
     csv_file = "../data_" + run_id + ".csv"
     with open(input_json) as fh:
         data_frame = pd.read_json(fh)
-        data_frame['district_full'] = pd.Series([getDistName(code) for code in data_frame['district']])
-        data_frame['dist_place'] = pd.Series([getDistPlace(dist, loc) for (dist, loc) in
+        data_frame['district_full'] = pd.Series([get_dist_name(code) for code in data_frame['district']])
+        data_frame['dist_place'] = pd.Series([get_dist_place(dist, loc) for (dist, loc) in
                                               zip(data_frame['district_full'], data_frame['location'])])
         print(data_frame.shape)
         print("============")
@@ -66,7 +80,7 @@ def convert_json_to_csv(input_json):
     return csv_file
 
 
-def getDistName(code):
+def get_dist_name(code):
     code_to_name = {
          'tvm': 'Thiruvananthapuram',
          'ptm': 'Pathanamthitta',
@@ -85,9 +99,12 @@ def getDistName(code):
     }
     return code_to_name[code]
 
+def feed_csv_to_solr(csv_file):
+    print("Feeding data to solr")
+    print(os.popen("/opt/solr/bin/post -c krescue10  " + csv_file).readlines());
 
-
-json_file = save_data_json()
-
-csv_file = convert_json_to_csv(json_file)
-
+json_file = save_data_json(fetch=False)
+print("Saved Json File: ", json_file)
+csv_file_out = convert_json_to_csv(json_file)
+print("Json converted to CSV: ", csv_file_out)
+feed_csv_to_solr(csv_file_out)
